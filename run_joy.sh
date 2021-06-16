847b5705526374718afeccd43b8b3c3a7911affb
SHOUGOUGOU <ya.sun.chn@gmail.com>
2021-06-16 18:06:20 +0800

    Create run_joy.sh

---
+++ run_joy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+node  jd_bean_change.js
+node  jd_bean_home.js
+
+node  jd_joy.js
+node  jd_joy_feedPets.js
+#node  jd_joy_help.js
+node  jd_joy_reward.js
+node  jd_joy_run.js
+node  jd_joy_steal.js
+
+node  jd_bean_change.js
+node  jd_bean_home.js
